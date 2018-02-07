1b94f50a0b8d5a32dc5e25ce218c5763c9386959
Jose Almeida <jose@MacBook-Pro.hitronhub.home>
2018-02-07 03:04:14 -0800

    Prep app to Heroku

--- server.js
+++ server.js
@@ -2,10 +2,32 @@ var express = require('express');
 
 //Create our app
 var app  = express();
+//Heroku port
+const PORT = process.env.PORT || 3000;
+
+
+app.use(function(req,res,next){
+    if(req.headers['x-forwarded-proto'] === 'http'){
+        next();
+    }
+    else{
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
 
 app.use(express.static('public'));
 
-app.listen(3001, function() {
+/*
+
+LOCAL PORT
+app.listen(3000, function() {
     console.log('Express server is up on port 3001');
+
+});
+*/
+
+app.listen(PORT, function() {
+    console.log('Express server is up on port ', PORT);
 
 });
