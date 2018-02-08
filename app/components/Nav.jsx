e12800916b4beda71aaab5691eb34db63599eae9
Jose Almeida <jose@MacBook-Pro.hitronhub.home>
2018-02-07 22:31:49 -0800

    Ädd style nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,33 +1,48 @@
 var React = require('react');
 var {Link,IndexLink}= require('react-router');
 
+var Nav = React.createClass({
+    onSearch: function(e){
+        e.preventDefault();
+        alert('Not yet wired up');
+    },
+    render: function() {
+        return(
+            <div className='top-bar'>
+                <div className='top-bar-left'>
+                    <ul className='menu'>
+                        <li className='menu-text'>
+                            React Weather App
+                        </li>
+                        <li>
+                            <IndexLink to="/" activeClassName='active' activeStyle={{fontWeight: 'bold'}} >Get Weather</IndexLink>
+                        </li>
+                        <li>
+                        <Link to="/About" activeClassName='active' activeStyle={{fontWeight: 'bold'}}>About</Link>
+                        </li>
+                        <li>
+                        <Link to="/Example" activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Example</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className='top-bar-right'>
+                    <form onSubmit={this.onSearch}>
+                        <ul className='menu'>
+                            <li>
+                                <input type="search" placeholder="Search wheater"/>
+                            </li>
+                            <li>
+                            <input type="submit" className="button" value="Get Weather"/>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+});
 
-// var Nav = React.createClass({
-//     render: function(){
-//         return(
-//             <div>
-//                 <h1>nav</h1>
-//                 <IndexLink to="/" activeClassName='active' activeStyle={{fontWeight: 'bold'}} >Get Weather</IndexLink>
-//                 <Link to="/About" activeClassName='active' activeStyle={{fontWeight: 'bold'}}>About</Link>
-//                 <Link to="/Example" activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Example</Link>
-//             </div>
-            
-//         );
-//     }
-
-// });
 
 
-var Nav = (props) => {
-    return(
-        <div>
-            <h1>nav</h1>
-            <IndexLink to="/" activeClassName='active' activeStyle={{fontWeight: 'bold'}} >Get Weather</IndexLink>
-            <Link to="/About" activeClassName='active' activeStyle={{fontWeight: 'bold'}}>About</Link>
-            <Link to="/Example" activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Example</Link>
-        </div>
-        
-    );
-};
 
 module.exports = Nav;
